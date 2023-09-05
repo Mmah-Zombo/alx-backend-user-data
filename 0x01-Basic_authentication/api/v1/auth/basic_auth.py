7fa3fbfd4418ed3a3d036bc7887bcb2611fa212b
M'mah Zombo <www.mariamzombo@gmail.com>
2023-09-05 21:47:37 +0100

    task10 again

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -67,7 +67,6 @@ class BasicAuth(Auth):
         users = User.search({'email': user_email})
         if not users or len(users) <= 0:
             return None
-        for user in users:
-            if user.is_valid_password(user_pwd):
-                return user
+        if users[0].is_valid_password(user_pwd):
+            return users[0]
         return None
