9a2762f55235c8f7083ea09699707296a6222547
M'mah Zombo <www.mariamzombo@gmail.com>
2023-09-05 19:50:03 +0100

    github

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -12,14 +12,10 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """returns a path"""
-        if path is None or not excluded_paths:
-            return True
-
-        # Check if the path is in the list of excluded_paths
-        if path in excluded_paths:
+        if path is not None and excluded_paths is not None:
             return False
 
-        return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """handles the authorization header"""
