f4d7a9ffff1365b6a64a63278bfa1ee2278b1567
M'mah Zombo <www.mariamzombo@gmail.com>
2023-09-05 21:21:41 +0100

    task8

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """module that contains the basic authentication class
 """
+import base64
 from .auth import Auth
 
 
@@ -21,3 +22,20 @@ class BasicAuth(Auth):
             return None
         else:
             return authorization_header.split(' ')[1]
+
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str) -> str:
+        """returns the decoded value of a Base64 string
+        base64_authorization_header:"""
+        if base64_authorization_header is None:
+            return None
+        if type(base64_authorization_header) is not str:
+            return None
+
+        try:
+            d_bytes = base64.b64decode(base64_authorization_header)
+            d_string = d_bytes.decode('utf-8')
+            return d_string
+        except Exception:
+            return None
