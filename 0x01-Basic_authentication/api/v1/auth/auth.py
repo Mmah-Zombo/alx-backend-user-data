28d608f60eeeb997e6b683d53ec22d1843781526
M'mah Zombo <www.mariamzombo@gmail.com>
2023-09-05 18:34:32 +0100

    created the authentication class with some basic functions

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""module that contains the authentication class
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """authentication class"""
+    def __init__(self) -> None:
+        pass
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """returns a path"""
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """handles the authorization header"""
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """gets the current user"""
+        return None
