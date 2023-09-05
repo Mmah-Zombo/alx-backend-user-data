42608e43cf43e4d95f6a4f2a58cfb1ef02ed696d
M'mah Zombo <www.mariamzombo@gmail.com>
2023-09-05 19:45:56 +0100

    gpt + me

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -12,12 +12,12 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """returns a path"""
-        if (path is None or path not in excluded_paths):
+        if (path is None):
             return True
         if excluded_paths is None or len(excluded_paths) == 0:
             return True
         if path in excluded_paths:
-            return False
+            return True
         return False
 
     def authorization_header(self, request=None) -> str:
