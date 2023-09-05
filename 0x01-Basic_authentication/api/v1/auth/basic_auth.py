c49e7a9ed0662067ff19dc063572b66574b5754a
M'mah Zombo <www.mariamzombo@gmail.com>
2023-09-05 20:53:38 +0100

    task6again

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
 """module that contains the basic authentication class
 """
-from auth import Auth
+from .auth import Auth
 
 
 class BasicAuth(Auth):
     """basic authentication class"""
-
-    def __init__(self) -> None:
-        super().__init__()
+    pass
