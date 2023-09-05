a0d2570ca8effe2ab1579002b96c85e3bb4e7aa4
M'mah Zombo <www.mariamzombo@gmail.com>
2023-09-05 20:54:20 +0100

    task6again

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -10,7 +10,7 @@ from flask_cors import (CORS, cross_origin)
 
 from api.v1.views import app_views
 from api.v1.auth.auth import Auth
-from api.v1.auth.auth import BaicAuth
+from api.v1.auth.basic_auth import BaicAuth
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
