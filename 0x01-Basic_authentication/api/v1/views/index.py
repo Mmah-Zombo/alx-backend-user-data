771c9012c6267e1564bf5daab80d7adc2cee82f5
M'mah Zombo <www.mariamzombo@gmail.com>
2023-09-05 18:05:54 +0100

    type annotation and correct route name

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -26,7 +26,7 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/api/v1/unauthorized', methods=['GET'], strict_slashes=False)
-def raise_error() -> str:
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def raise_error() -> None:
     """raises an error by using abort"""
     abort(401)
