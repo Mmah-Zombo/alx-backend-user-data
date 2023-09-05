e7dc762fbeb04ae5b245a91e138035dadeeec63a
M'mah Zombo <www.mariamzombo@gmail.com>
2023-09-05 18:01:36 +0100

    added a new endpoint in index.py

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,9 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/api/v1/unauthorized', methods=['GET'], strict_slashes=False)
+def raise_error() -> str:
+    """raises an error by using abort"""
+    abort(401)
