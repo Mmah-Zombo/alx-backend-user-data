b31874d863e43e61b4b0c78959f33bb3620eb7af
M'mah Zombo <www.mariamzombo@gmail.com>
2023-09-06 16:43:27 +0000

    finding the error

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -12,28 +12,27 @@ class SessionAuth(Auth):
 
     user_id_by_session_id = {}
 
-    def create_session(self, user_id: str = None) -> str:
-        """creates a Session ID for a <user_id>"""
-        if user_id is None:
-            return None
-        if type(user_id) is not str:
-            return None
-        else:
-            session_id = str(uuid.uuid4())
-            self.user_id_by_session_id[session_id] = user_id
-            return session_id
+    # def create_session(self, user_id: str = None) -> str:
+    #     """creates a Session ID for a <user_id>"""
+    #     if user_id is None:
+    #         return None
+    #     if type(user_id) is not str:
+    #         return None
+    #     else:
+    #         session_id = str(uuid.uuid4())
+    #         self.user_id_by_session_id[session_id] = user_id
+    #         return session_id
 
-    def user_id_for_session_id(self, session_id: str = None) -> str:
-        """returns a User ID based on a Session ID"""
-        if session_id is None:
-            return None
-        if type(session_id) is not str:
-            return None
-        return self.user_id_by_session_id.get(session_id)
+    # def user_id_for_session_id(self, session_id: str = None) -> str:
+    #     """returns a User ID based on a Session ID"""
+    #     if session_id is None:
+    #         return None
+    #     if type(session_id) is not str:
+    #         return None
+    #     return self.user_id_by_session_id.get(session_id)
 
-    def current_user(self, request=None):
-        """returns a User instance based on a cookie value"""
-        cookie = self.session_cookie(request)
-        user_id = self.user_id_for_session_id(cookie)
-        user = User.get(user_id)
-        return user
+    # def current_user(self, request=None) -> User:
+    #     """returns a User instance based on a cookie value"""
+    #     cookie = self.session_cookie(request)
+    #     user_id = self.user_id_for_session_id(cookie)
+    #     return User.get(user_id)
