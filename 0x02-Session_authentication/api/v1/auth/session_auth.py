8098fbf52b230ed32ac431aa6015c283e40c2c56
M'mah Zombo <www.mariamzombo@gmail.com>
2023-09-06 15:49:14 +0000

    task2

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,5 +1,6 @@
 """module that contains the session authentication class
 """
+import uuid
 from .auth import Auth
 
 
@@ -7,3 +8,16 @@ class SessionAuth(Auth):
     """the session authentication class"""
     def __init__(self) -> None:
         super().__init__()
+
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """creates a Session ID for a <user_id>"""
+        if user_id is None:
+            return None
+        if type(user_id) is not str:
+            return None
+        else:
+            session_id = str(uuid.uuid4())
+            self.user_id_by_session_id[session_id] = user_id
+            return session_id
