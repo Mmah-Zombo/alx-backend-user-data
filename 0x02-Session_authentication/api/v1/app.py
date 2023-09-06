a0e190fcf2176ff79ef82a5a13ad394af316c999
M'mah Zombo <www.mariamzombo@gmail.com>
2023-09-06 17:26:14 +0000

    task5 redo

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -50,12 +50,13 @@ def before():
     """gets excuted first then a request is handeled"""
     if auth is not None:
         excluded = ['/api/v1/status/', '/api/v1/unauthorized/',
-                    '/api/v1/forbidden/']
+                    '/api/v1/forbidden/', '/api/v1/auth_session/login/']
         checker = auth.require_auth(request.path, excluded)
         if checker:
             auth_header = auth.authorization_header(request)
             request.current_user = auth.current_user(request)
-            if auth_header is None:
+            auth_session = auth.session_cookie(request)
+            if auth_header is None and auth_session is None:
                 abort(401)
             if request.current_user is None:
                 abort(403)
