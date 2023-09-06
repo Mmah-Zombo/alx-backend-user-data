5b22908adaacafcafa89af2523c8dbab6cfa99b1
M'mah Zombo <www.mariamzombo@gmail.com>
2023-09-06 15:31:08 +0000

    task1

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,9 @@
+"""module that contains the session authentication class
+"""
+from .auth import Auth
+
+
+class SessionAuth(Auth):
+    """the session authentication class"""
+    def __init__(self) -> None:
+        super().__init__()
