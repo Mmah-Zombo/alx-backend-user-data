531cedda3b0f9b8a75c5242690ebb2524fbdfe1d
M'mah Zombo <www.mariamzombo@gmail.com>
2023-09-07 19:12:18 +0000

    task0

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,5 +5,6 @@ import re
 
 def filter_datum(fields, redaction, message, separator):
     """the filter_datum function"""
-    regex = re.compile(f'({separator.join(map(re.escape, fields))})')
-    return regex.sub(redaction, message)
+    regex = '|'.join(map(re.escape, fields))
+    return re.sub(f'({regex}){re.escape(separator)}',
+                  redaction + separator, message)
