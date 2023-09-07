e3d027a4f1f8117ae063a3cf2b19e7bb47ebdd7f
M'mah Zombo <www.mariamzombo@gmail.com>
2023-09-07 19:15:30 +0000

    task1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,10 +1,26 @@
 #!/usr/bin/env python3
-"""regex-ing"""
+import logging
 import re
 
 
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        message = super(RedactingFormatter,
+                        self).format(record)
+        return filter_datum(self.fields,
+                            self.REDACTION, message, self.SEPARATOR)
+
+
 def filter_datum(fields, redaction, message, separator):
-    """the filter_datum function"""
-    regex = '|'.join(map(re.escape, fields))
-    return re.sub(f'({regex}){re.escape(separator)}',
-                  redaction + separator, message)
+    regex = re.compile(f'({separator.join(map(re.escape, fields))})')
+    return regex.sub(redaction, message)
